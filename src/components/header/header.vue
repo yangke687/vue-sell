26c84c0c417c7417f68254c62e084c445202e191
Yang Ke <saint_yk@163.com>
2017-02-22 21:26:31 +0800

    header popup layer flex layout

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -33,7 +33,19 @@
 			<div class="detail-wrapper clearfix">
 				<div class="detail-main">
 					<h1 class="name">{{ seller.name }}</h1>
-					<star :size="48" :score="seller.score"></star>
+					<div class="star-wrapper">
+						<star :size="48" :score="seller.score"></star>
+					</div>
+					<div class="title">
+						<div class="line"></div>
+						<div class="text">优惠信息</div>
+						<div class="line"></div>
+					</div>
+					<div class="title">
+						<div class="line"></div>
+						<div class="text">商家公告</div>
+						<div class="line"></div>
+					</div>
 				</div>
 			</div>
 			<div class="detail-close">
@@ -204,6 +216,23 @@
 						text-align: center
 						font-size: 16px
 						font-weight: 700
+					.star-wrapper
+						margin-top: 18px
+						padding: 2px 0px
+						text-align: center
+					.title
+						display: flex
+						width: 80%
+						margin: 30px auto 24px auto
+						.line
+							flex: 1
+							position: relative
+							top: -6px
+							border-bottom: 1px solid rgba(255,255,255,.2)
+						.text
+							padding: 0px 12px
+							font-size: 14px
+							font-weight: 700
 			.detail-close
 				position: relative
 				width: 32px

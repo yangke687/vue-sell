ba972d9ad230ebb49b323ae2b34db483983f1864
Yang Ke <saint_yk@163.com>
2017-02-27 21:43:12 +0800

    shopcart page, decrease btn clicked animation style

--- src/components/cartcontrol/cartcontrol.vue
+++ src/components/cartcontrol/cartcontrol.vue
@@ -1,6 +1,8 @@
 <template>
 	<div class="cartcontrol">
-		<div class="cart-decrease icon-remove_circle_outline" v-show="food.count>0" @click="decreaseCart"></div>
+		<div class="cart-decrease" v-show="food.count>0" @click="decreaseCart" transition="move">
+      <span class="inner icon-remove_circle_outline"></span>
+    </div>
 		<div class="cart-count" v-show="food.count>0">{{ food.count }}</div>
 		<div class="cart-increase icon-add_circle" @click="addCart"></div>
 	</div>
@@ -40,7 +42,26 @@
 <style lang="stylus" rel="stylesheet/stylus">
   .cartcontrol
     font-size: 0
-    .cart-decrease,.cart-increase
+    .cart-decrease
+      display:inline-block
+      padding: 6px
+      transition: all 0.4s linear
+      &.move-transition
+        opacity: 1
+        transform: translate3D(0,0,0)
+        .inner
+          display: inline-block
+          line-height: 24px
+          font-size: 24px
+          color: rgb(0,160,220)
+          transition: all 0.4s linear
+          transform: rotate(0)
+      &.move-enter,&.move-leave
+        opacity: 0
+        transform: translate3D(24px,0,0)
+        .inner
+          transform: rotate(180deg)
+    .cart-increase
       display:inline-block
       padding: 6px
       line-height: 24px

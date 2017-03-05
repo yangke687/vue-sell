9827572e8eecc5ac03c939b17914ce65b1c1440a
Yang Ke <saint_yk@163.com>
2017-03-05 21:26:34 +0800

    food iamge/detail page, detail,price part style

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -8,6 +8,16 @@
 						<i class="icon-arrow_lift"></i>
 					</div>
 				</div>
+				<div class="content">
+					<h1 class="title">{{food.name}}<h1>
+					<div class="detail">
+						<span class="sell-count">月售{{food.sellCount}}份</span>
+						<span class="rating">好评率{{food.rating}}％</span>
+					</div>
+					<div class="price">
+						<span class="now">¥{{food.price}}</span><span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</sapn>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -61,13 +71,43 @@
 				left: 0
 				width: 100%
 				height: 100%
-		.back
-			position: absolute
-			top: 10px
-			left: 0
-			.icon-arrow_lift
-				display: block
-				padding: 10px
-				font-size: 20px
-				color: #FFF
+			.back
+				position: absolute
+
+				top: 10px
+				left: 0
+				.icon-arrow_lift
+					display: block
+					padding: 10px
+					font-size: 20px
+					color: #FFF
+		.content
+			padding: 18px
+			.title
+				line-height: 14px
+				margin-bottom: 8px
+				font-size: 14px
+				font-weight: 700
+				color: rgb(7,17,27)
+			.detail
+				margin-bottom: 18px
+				line-height: 10px
+				font-size: 0
+				height: 10px
+				.sell-count,.rating
+					font-size: 10px
+					color: rgb(147,153,159)
+				.sell-count
+					margin-right: 12px
+			.price
+				font-weight: 700
+				line-height: 24px
+				.now
+					margin-right: 8px
+					font-size: 14px
+					color: rgb(240,20,20)
+				.old
+					text-decoration: line-through
+					font-size: 10px
+					color: rgb(147,153,159)
 </style>

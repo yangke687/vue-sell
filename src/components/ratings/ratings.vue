921aec5e37ae7aff960dcd342a9d2bcc9d013b18
Yang Ke <saint_yk@163.com>
2017-03-14 22:28:16 +0800

    raintgs component, overview-left

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,7 +1,61 @@
 <template>
-	<div>I am ratings page</div>
+	<div class="ratings">
+		<div class="ratings-content">
+			<div class="overview">
+				<div class="overview-left">
+					<h1 class="score">{{seller.score}}</h1>
+					<div class="title">综合评分</div>
+					<div class="rank">高于周边商家{{seller.rankRate}}%</div>
+				</div>
+				<div class="overview-right">
+
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
+
 <script type="text/ecmascript-6">
-	export default {};
+	export default {
+		props: {
+			seller: {
+				type: Object
+			}
+		}
+	};
 </script>
-<style lang="stylus" rel="stylesheet/stylus"></style>
+
+<style lang="stylus" rel="stylesheet/stylus">
+	.ratings
+		position: absolute
+		top: 174px
+		bottom: 0
+		left: 0
+		right: 0
+		overflow: hidden
+		.overview
+			display: flex
+			padding: 18px 0
+			.overview-left
+				flex: 0 0 137px
+				border-right: 1px solid rgba(7,17,27,.1)
+				text-align: center
+				.score
+					margin-bottom: 6px
+					line-height: 28px
+					color: rgb(255,153,0)
+					font-size: 24px
+				.title
+					margin-bottom: 8px
+					line-height: 12px
+					font-size: 12px
+					color: rgb(7,17,27)
+				.rank
+					line-height: 10px
+					font-size: 10px
+					color: rgb(147,153,159)
+			.overview-right
+				flex: 1
+				padding-left: 24px
+
+</style>

ef4ae16943830e085b53ec5d09c39095b6cd98f7
Yang Ke <saint_yk@163.com>
2017-03-14 23:19:59 +0800

    ratings component, overview-right

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -8,7 +8,20 @@
 					<div class="rank">高于周边商家{{seller.rankRate}}%</div>
 				</div>
 				<div class="overview-right">
-
+					<div class="score-wrapper">
+						<span class="title">服务态度</span>
+						<star :score="seller.serviceScore" :size="36"></star>
+						<span class="score">{{seller.serviceScore}}</span>
+					</div>
+					<div class="score-wrapper">
+						<span class="title">商品评分</span>
+						<star :score="seller.foodScore" :size="36"></star>
+						<span class="score">{{seller.foodScore}}</span>
+					</div>
+					<div class="delivery-time">
+						<span class="title">送达时间</span>
+						<span class="delivery">{{seller.deliveryTime}}分钟</span>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -16,11 +29,15 @@
 </template>
 
 <script type="text/ecmascript-6">
+	import star from 'components/star/star.vue';
 	export default {
 		props: {
 			seller: {
 				type: Object
 			}
+		},
+		components: {
+			star: star
 		}
 	};
 </script>
@@ -38,6 +55,7 @@
 			padding: 18px 0
 			.overview-left
 				flex: 0 0 137px
+				padding: 6px 0
 				border-right: 1px solid rgba(7,17,27,.1)
 				text-align: center
 				.score
@@ -56,6 +74,35 @@
 					color: rgb(147,153,159)
 			.overview-right
 				flex: 1
-				padding-left: 24px
-
+				padding: 6px 0 6px 24px
+				.score-wrapper
+					line-height: 18px
+					margin-bottom: 8px
+					font-size: 0
+					.title
+						display: inline-block
+						vertical-align: top
+						font-size: 12px
+						color: rgb(7,17,27)
+						line-height: 18px
+					.star
+						display: inline-block
+						margin: 0 12px
+						vertical-align: top
+					.score
+						display: inline-block
+						vertical-align: top
+						font-size: 12px
+						line-height: 18px
+						color: rgb(255,153,0)
+				.delivery-time
+					font-size: 0
+					.title
+						font-size: 12px
+						color: rgb(7,17,27)
+						line-height: 18px
+					.delivery
+						font-size: 12px
+						color: rgb(147,153,159)
+						padding-left: 12px
 </style>

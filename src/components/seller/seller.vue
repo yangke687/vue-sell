f6a7313cea1b23968bba4201245dbdfe84d2521f
Yang Ke <saint_yk@163.com>
2017-03-19 23:20:14 +0800

    seller.vue, seller overview style

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -11,15 +11,21 @@
 				<ul class="remark">
 					<li class="block">
 						<h2>起送价</h2>
-						<span class="stress">{{seller.minPrice}}</span>元
+						<div class="content">
+							<span class="stress">{{seller.minPrice}}</span>元
+						</div>
 					</li>
 					<li class="block">
 						<h2>商家配送</h2>
-						<span class="stress">{{seller.deliveryPrice}}</span>元
+						<div class="content">
+							<span class="stress">{{seller.deliveryPrice}}</span>元
+						</div>
 					</li>
 					<li class="block">
 						<h2>平均配送时间</h2>
-						<span class="stress">{{seller.deliveryTime}}</span>分钟
+						<div class="content">
+							<span class="stress">{{seller.deliveryTime}}</span>分钟
+						</div>
 					</li>
 				</ul>
 			</div>
@@ -39,4 +45,57 @@
 		}
 	};
 </script>
-<style lang="stylus" rel="stylesheet/stylus"></style>
+<style lang="stylus" rel="stylesheet/stylus">
+
+@import "../../common/stylus/mixin.styl"
+
+	.seller
+		position: absolute
+		top: 174px
+		bottom: 0
+		left: 0
+		right: 0
+		overflow: hidden
+		.overview
+			padding: 18px
+			.title
+				margin-bottom: 8px
+				line-height: 14px
+				color: rgb(7,17,27)
+				font-size: 14px
+			.desc
+				padding-bottom: 18px
+				line-height: 18px
+				font-size: 0px
+				border-1px(rgba(7,17,27,.1))
+				.star
+					display: inline-block
+					margin-right: 8px
+					vertical-align: top
+				.text
+					display: inline-block
+					margin-right: 12px
+					vertical-align: top
+					font-size: 10px
+					color: rgb(77,85,93)
+			.remark
+				display: flex
+				padding-top: 18px
+				.block
+					flex: 1
+					text-align: center
+					border-right: 1px solid rgba(7,17,27,.1)
+					&:last-child
+						border: none
+					h2
+						margin-bottom: 4px
+						line-height: 10px
+						font-size: 10px
+						color: rgb(147,153,159)
+					.content
+						line-height: 24px
+						font-size: 10px
+						color: rgb(7,17,27)
+						.stress
+							font-size: 24px
+</style>

47c5b284b7076504c4e6bfea0bf98dd6fe0f7330
Yang Ke <saint_yk@163.com>
2017-03-29 21:48:35 +0800

    seller.vue, pics-list

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -42,6 +42,17 @@
 					</li>
 				</ul>
 			</div>
+			<split></split>
+			<div class="pics">
+				<h2 class="title">商家实景</h2>
+				<div class="pic-wrapper" v-el:pic-wrapper>
+					<ul class="pic-list" v-el:pic-list>
+						<li class="pic-item" v-for="pic in seller.pics">
+							<img :src="pic" alt="" width="120" height="90">
+						</li>
+					</ul>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
@@ -66,7 +77,9 @@
 		},
 		watch: {
 			'seller'(){
+				console.log('watch');
 				this._initScroll();
+				this._initPics();
 			}
 		},
 		methods: {
@@ -78,10 +91,29 @@
 				} else {
 					this.scroll.refresh();
 				}
+			},
+			_initPics(){
+				if (this.seller.pics) {
+					let picWidth = 120;
+					let margin = 6;
+					let width = (picWidth + margin) * this.seller.pics.length - margin;
+					this.$els.picList.style.width = width + 'px';
+					this.$nextTick(() => {
+						if (!this.picSCroll) {
+							this.picScroll = new BScroll(this.$els.picWrapper, {
+								scrollX: true,
+								eventPassthrough: 'vertical'
+							});
+						} else {
+							this.picScroll.refresh();
+						}
+					});
+				}
 			}
 		},
 		ready() {
 			this._initScroll();
+			this._initPics();
 		}
 	};
 </script>
@@ -181,5 +213,24 @@
 						font-size: 12px
 						line-height: 16px
 						color: rgb(7,17,27)
-
+		.pics
+			padding: 18px
+			.title
+				margin-bottom: 12px
+				line-height: 14px
+				color: rgb(7,17,27)
+				font-size: 14px
+			.pic-wrapper
+				width: 100%
+				overflow: hidden
+				white-space: nowrap
+				.pic-list
+					font-size: 0px
+					.pic-item
+						display: inline-block
+						margin-right: 6px
+						width: 120px
+						height: 90px
+						&:last-child
+							margin: 0
 </style>
